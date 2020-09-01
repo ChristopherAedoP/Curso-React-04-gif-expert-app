c11c173d73238fdccc0f5a9c6b2e43e2189f64ce
Christopher Aedo <Christopher.aedo.p@gmail.com>
2020-09-01 17:07:57 -0400

    docs agregado para publicar

---
+++ docs/precache-manifest.8407bb8a834536b616b3ac14186768f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a81bec64454e9280a26cedd0a4f6ad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13ce559d8682f3a618ec",
+    "url": "/static/css/main.7d50b8fb.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ce559d8682f3a618ec",
+    "url": "/static/js/main.af213953.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
